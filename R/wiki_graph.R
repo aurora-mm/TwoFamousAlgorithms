f760ef7757eb4a0919ac422750ce31f0086d7ca2
LRF <linn.friberg@pm.me>
2024-09-21 22:24:15 +0200

    Add wiki_graph dataset

---
+++ R/wiki_graph.R
@@ -0,0 +1,11 @@
+#' A test dataset for Dijkstra's algorithm implementation
+#'
+#' @format ## `wiki-graph`
+#' A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1, v2}{Edges of the graph}
+#'   \item{w}{Weight of the edge}
+#'   ...
+#' }
+#' @source <https://en.wikipedia.org/wiki/Dijkstra%27salgorithm>
+"wiki_graph"
--- twofamousalgorithms/R/twofamousalgorithms-package.R
+++ twofamousalgorithms/R/twofamousalgorithms-package.R
@@ -12,4 +12,14 @@
 #' path from the initial node to every other node in the graph.
 #'
 
-
+#' A test dataset for Dijkstra's algorithm implementation
+#'
+#' @format ## `wiki-graph`
+#' A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1, v2}{Edges of the graph}
+#'   \item{w}{Weight of the edge}
+#'   ...
+#' }
+#' @source <https://en.wikipedia.org/wiki/Dijkstra%27salgorithm>
+"wiki_graph"
