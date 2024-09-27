051ee62b9ea68b25c178be039c01c9cc75d466b0
LRF <linn.friberg@pm.me>
2024-09-27 19:29:39 +0200

    Update twofamousalgorithms-package.R

--- R/twofamousalgorithms-package.R
+++ R/twofamousalgorithms-package.R
@@ -3,13 +3,18 @@
 #' 
 #' twofamousalgorithms: Implementations of the Euclidean algorithm and Dijkstra's algorithm
 #'
-#' This package includes implementations of two famous algorithms: the Euclidean algorithm 
-#' for finding the greatest common divisor of two integers and Dijkstra algorithm for finding 
-#' the shortest path in a graph.
+#' This R package features implementations of the Euclidean algorithm, 
+#' which calculates the greatest common divisor of two integers, and Dijkstra's 
+#' algorithm, used for finding the shortest path in a graph. This package is a 
+#' part of the coursework for the Advanced Programming in R course (732A94) at 
+#' Linköping University.
 #' 
 #' @section dijkstra:
 #' The algorithm takes a graph and an initial node and calculates the shortest
 #' path from the initial node to every other node in the graph.
 #'
-
+#' @section euclidean
+#' The algorithm takes two numeric scalars and applies the Euclidean
+#' algorithm to it. In the case these two numeric scalars are
+#' integers the greatest common divisor is yielded.
 
